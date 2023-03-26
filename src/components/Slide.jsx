8f86b04c81b1d451d170f7857815f01e046751ab
Brandon Bhangari <b6_10_94_furlonge@hotmail.com>
2023-03-25 22:13:55 -0400

    implementing tab navigation for accessibility

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -6,23 +6,33 @@ import classes from "./Slide.module.css";
 function Slide() {
   const [modal, setModal] = useState(false);
   const [slide, setSlide] = useState(1);
+  const [tab, setTab] = useState(1);
 
   const modalHandler = () => {
     setModal((prev) => !prev);
   };
 
-  const prevSlideHandler = () => {
+  const changeTab = (e) => {
+    if (e.keyCode === 37) {
+      setTab(1);
+    }
+    if (e.keyCode === 39) {
+      setTab(2);
+    }
+  };
+
+  const prevSlideHandler = (e) => {
     if (slide > 1) {
       setSlide((prev) => prev - 1);
-    } else {
+    } else if (slide === 1) {
       setSlide(3);
     }
   };
 
-  const nextSlideHandler = () => {
+  const nextSlideHandler = (e) => {
     if (slide < 3) {
       setSlide((prev) => parseInt(prev) + 1);
-    } else {
+    } else if (slide === 3) {
       setSlide(1);
     }
   };
@@ -92,18 +102,52 @@ function Slide() {
               <div className={classes.line}></div>
             </li>
           </ul>
-          <div className={classes.actions}>
-            <div className={classes.left} onClick={prevSlideHandler}>
+          <div
+            className={classes.actions}
+            onKeyDown={changeTab}
+            role="tablist"
+            aria-label="slider"
+          >
+            <button
+              className={classes.left}
+              role="tab"
+              onClick={prevSlideHandler}
+              tabIndex={tab === 1 ? 0 : -1}
+              aria-selected={tab === 1 ? "true" : "false"}
+              aria-controls="previous-slide"
+            >
               <img src="/images/icon-angle-left.svg" alt="angle left icon" />
-            </div>
-            <div className={classes.right} onClick={nextSlideHandler}>
+            </button>
+            <button
+              className={classes.right}
+              role="tab"
+              onClick={nextSlideHandler}
+              tabIndex={tab === 2 ? 0 : -1}
+              aria-selected={tab === 2 ? "true" : "false"}
+              aria-controls="next-slide"
+            >
               <img src="/images/icon-angle-right.svg" alt="angle right icon" />
-            </div>
+            </button>
           </div>
         </div>
 
         <div className={classes.info}>
-          <h2 hidden={slide === 1 ? false : true}>
+          <div
+            role="tabpanel"
+            id="slide-one"
+            tabIndex="0"
+            hidden={slide === 1 ? false : true}
+          >
+            <h2>Discover innovative ways to decorate</h2>
+            <p>
+              We provide unmatched quality, comfort, and style for property
+              owners across the country. Our experts combine form and function
+              in bringing your vision to life. Create a room in your own style
+              with our collection and make your property a reflection of you and
+              what you love.
+            </p>
+          </div>
+          {/* <h2 hidden={slide === 1 ? false : true}>
             Discover innovative ways to decorate
           </h2>
           <p hidden={slide === 1 ? false : true}>
@@ -112,8 +156,23 @@ function Slide() {
             bringing your vision to life. Create a room in your own style with
             our collection and make your property a reflection of you and what
             you love.
-          </p>
-          <h2 hidden={slide === 2 ? false : true}>
+          </p> */}
+          <div
+            role="tabpanel"
+            id="slide-two"
+            tabIndex="0"
+            hidden={slide === 2 ? false : true}
+          >
+            <h2>We are available all across the globe</h2>
+            <p>
+              With stores all over the world, it's easy for you to find
+              furniture for your home or place of business. Locally, we’re in
+              most major cities throughout the country. Find the branch nearest
+              you using our store locator. Any questions? Don't hesitate to
+              contact us today.
+            </p>
+          </div>
+          {/* <h2 hidden={slide === 2 ? false : true}>
             We are available all across the globe
           </h2>
           <p hidden={slide === 2 ? false : true}>
@@ -121,8 +180,23 @@ function Slide() {
             for your home or place of business. Locally, we’re in most major
             cities throughout the country. Find the branch nearest you using our
             store locator. Any questions? Don't hesitate to contact us today.
-          </p>
-          <h2 hidden={slide === 3 ? false : true}>
+          </p> */}
+          <div
+            role="tabpanel"
+            id="slide-three"
+            tabIndex="0"
+            hidden={slide === 3 ? false : true}
+          >
+            <h2>Manufactured with the best materials</h2>
+            <p>
+              Our modern furniture store provide a high level of quality. Our
+              company has invested in advanced technology to ensure that every
+              product is made as perfect and as consistent as possible. With
+              three decades of experience in this industry, we understand what
+              customers want for their home and office.
+            </p>
+          </div>
+          {/* <h2 hidden={slide === 3 ? false : true}>
             Manufactured with the best materials
           </h2>
           <p hidden={slide === 3 ? false : true}>
@@ -131,7 +205,7 @@ function Slide() {
             product is made as perfect and as consistent as possible. With three
             decades of experience in this industry, we understand what customers
             want for their home and office.
-          </p>
+          </p> */}
           <a href="#" className={classes.shop}>
             <p>Shop now</p>
             <svg width="40" height="12" xmlns="http://www.w3.org/2000/svg">

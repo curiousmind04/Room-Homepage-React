43dda6d2bca514f469457fc41c56fd426780bb19
Brandon Bhangari <b6_10_94_furlonge@hotmail.com>
2023-03-26 22:20:43 -0400

    used button elements for slider arrows instead of divs

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -6,19 +6,9 @@ import classes from "./Slide.module.css";
 function Slide() {
   const [modal, setModal] = useState(false);
   const [slide, setSlide] = useState(1);
-  const [tab, setTab] = useState(1);
 
   const modalHandler = () => {
     setModal((prev) => !prev);
-  };
-
-  const changeTab = (e) => {
-    if (e.keyCode === 37) {
-      setTab(1);
-    }
-    if (e.keyCode === 39) {
-      setTab(2);
-    }
   };
 
   const prevSlideHandler = (e) => {
@@ -102,29 +92,18 @@ function Slide() {
               <div className={classes.line}></div>
             </li>
           </ul>
-          <div
-            className={classes.actions}
-            onKeyDown={changeTab}
-            role="tablist"
-            aria-label="slider"
-          >
+          <div className={classes.actions}>
             <button
               className={classes.left}
-              role="tab"
               onClick={prevSlideHandler}
-              tabIndex={tab === 1 ? 0 : -1}
-              aria-selected={tab === 1 ? "true" : "false"}
-              aria-controls="previous-slide"
+              // tabIndex="0"
             >
               <img src="/images/icon-angle-left.svg" alt="angle left icon" />
             </button>
             <button
               className={classes.right}
-              role="tab"
               onClick={nextSlideHandler}
-              tabIndex={tab === 2 ? 0 : -1}
-              aria-selected={tab === 2 ? "true" : "false"}
-              aria-controls="next-slide"
+              // tabIndex="0"
             >
               <img src="/images/icon-angle-right.svg" alt="angle right icon" />
             </button>
@@ -132,12 +111,7 @@ function Slide() {
         </div>
 
         <div className={classes.info}>
-          <div
-            role="tabpanel"
-            id="slide-one"
-            tabIndex="0"
-            hidden={slide === 1 ? false : true}
-          >
+          <div id="slide-one" tabIndex="0" hidden={slide === 1 ? false : true}>
             <h2>Discover innovative ways to decorate</h2>
             <p>
               We provide unmatched quality, comfort, and style for property
@@ -147,22 +121,7 @@ function Slide() {
               what you love.
             </p>
           </div>
-          {/* <h2 hidden={slide === 1 ? false : true}>
-            Discover innovative ways to decorate
-          </h2>
-          <p hidden={slide === 1 ? false : true}>
-            We provide unmatched quality, comfort, and style for property owners
-            across the country. Our experts combine form and function in
-            bringing your vision to life. Create a room in your own style with
-            our collection and make your property a reflection of you and what
-            you love.
-          </p> */}
-          <div
-            role="tabpanel"
-            id="slide-two"
-            tabIndex="0"
-            hidden={slide === 2 ? false : true}
-          >
+          <div id="slide-two" tabIndex="0" hidden={slide === 2 ? false : true}>
             <h2>We are available all across the globe</h2>
             <p>
               With stores all over the world, it's easy for you to find
@@ -172,17 +131,7 @@ function Slide() {
               contact us today.
             </p>
           </div>
-          {/* <h2 hidden={slide === 2 ? false : true}>
-            We are available all across the globe
-          </h2>
-          <p hidden={slide === 2 ? false : true}>
-            With stores all over the world, it's easy for you to find furniture
-            for your home or place of business. Locally, we’re in most major
-            cities throughout the country. Find the branch nearest you using our
-            store locator. Any questions? Don't hesitate to contact us today.
-          </p> */}
           <div
-            role="tabpanel"
             id="slide-three"
             tabIndex="0"
             hidden={slide === 3 ? false : true}
@@ -196,16 +145,6 @@ function Slide() {
               customers want for their home and office.
             </p>
           </div>
-          {/* <h2 hidden={slide === 3 ? false : true}>
-            Manufactured with the best materials
-          </h2>
-          <p hidden={slide === 3 ? false : true}>
-            Our modern furniture store provide a high level of quality. Our
-            company has invested in advanced technology to ensure that every
-            product is made as perfect and as consistent as possible. With three
-            decades of experience in this industry, we understand what customers
-            want for their home and office.
-          </p> */}
           <a href="#" className={classes.shop}>
             <p>Shop now</p>
             <svg width="40" height="12" xmlns="http://www.w3.org/2000/svg">
